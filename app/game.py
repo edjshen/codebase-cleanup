4f057eee219c5245e336552a7d0317b091b46c8b
edjshen <75459293+edjshen@users.noreply.github.com>
2022-05-04 14:45:39 -0400

    refactored winner determination logic

--- app/game.py
+++ app/game.py
@@ -4,27 +4,44 @@
 
 from random import choice
 
-#
-# USER SELECTION
-#
 
-u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
-print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
-    print("OOPS, TRY AGAIN")
-    exit()
 
-#
-# COMPUTER SELECTION
-#
-
-c = choice(["rock", "paper", "scissors"])
-print("COMPUTER CHOICE:", c)
 
 #
 # DETERMINATION OF WINNER
 #
 
+def validate(u,c):
+    winner = -1
+    possible = {"rock":"rock","paper":"paper","scissors":"scissors","rock":"scissors","paper":"rock","scissors":"paper"}
+    if u == c:
+        print("It's a tie")
+    elif possible[u] == c:   
+        print("You win")
+    elif possible[c] == u:        
+        print("You lose")
+    else:
+        print("You broke the game \n")
+
+if __name__ == "__main__":
+    
+#
+# USER SELECTION
+#
+    u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
+    print("USER CHOICE:", u)
+    if u not in ["rock", "paper", "scissors"]:
+        print("OOPS, TRY AGAIN")
+        exit()
+        
+#
+# COMPUTER SELECTION
+#
+    c = choice(["rock", "paper", "scissors"])
+    print("COMPUTER CHOICE:", c)
+    validate(u,c)
+
+"""
 if u == "rock" and c == "rock":
     print("It's a tie!")
 elif u == "rock" and c == "paper":
@@ -45,3 +62,5 @@ elif u == "scissors" and c == "paper":
     print("The user wins")
 elif u == "scissors" and c == "scissors":
     print("It's a tie!")
+    """
+    

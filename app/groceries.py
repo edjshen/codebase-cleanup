4135127cf0ecf2952152b16da094c668f7f2e9d3
edjshen <75459293+edjshen@users.noreply.github.com>
2022-05-04 15:17:27 -0400

    refactored usd formatting, average, and csv function

--- app/groceries.py
+++ app/groceries.py
@@ -6,45 +6,73 @@
 #products = products_df.to_dict("records")
 
 import os
-
-# checks to see if a products.csv file exists. If not, it uses the default
-if os.path.isfile(os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")) == True:
-    print("USING CUSTOM PRODUCTS CSV FILE...")
-    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
-else:
-    print("USING DEFAULT PRODUCTS CSV FILE...")
-    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "default_products.csv")
-
-
-
+import statistics
 from pandas import read_csv
 
-#reads the csv file into products variable
-products = read_csv(csv_filepath)
-#pandas transforms the data into a list of dictionaries
-products = products.to_dict('records')
 
 
+# to_usd function
+"""
+converts a number to currency format with usd sign and two decimal places
+
+@param: number to be converted to currency format
+
+examples:
+    to_usd(546.55)
+
+"""
+def to_usd(my_price):
+    return f"${my_price:,.2f}"
+
+
+"""
+
+gives the average value of a list or dataframe
+
+@param: list or data frame to be averaged
+
+examples: 
+    avg_price(all_prices)
+
+"""
+def avg(csv):
+    out = statistics.mean(csv)
+    return out
+    
 
 # PRINTED INVENTORY REPORT
+if __name__ == "__main__":
+    # checks to see if a products.csv file exists. If not, it uses the default
+    if os.path.isfile(os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")) == True:
+        print("USING CUSTOM PRODUCTS CSV FILE...")
+        csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
+    else:
+        print("USING DEFAULT PRODUCTS CSV FILE...")
+        csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "default_products.csv")
+    
+    #reads the csv file into products variable
+    products = read_csv(csv_filepath)
+    #pandas transforms the data into a list of dictionaries
+    products = products.to_dict('records')
+    
+    all_prices = []
+    print("---------")
+    print("THERE ARE", len(products), "PRODUCTS:")
+    print("---------")
+    
+    for p in products:
+        print("..." + p["name"] + " " + to_usd(p["price"]))
+        all_prices.append(float(p["price"]))       
+    
+    avg_price = avg(all_prices)
+    
+    print("---------")
+    print("AVERAGE PRICE:", to_usd(avg_price))
 
-print("---------")
-print("THERE ARE", len(products), "PRODUCTS:")
-print("---------")
-
-for p in products:
-    print("..." + p["name"] + "   " + '${:,.2f}'.format(p["price"]))
 
 
-all_prices = []
-for p in products:
-    all_prices.append(float(p["price"]))
 
-import statistics
-avg_price = statistics.median(all_prices)
 
-print("---------")
-print("AVERAGE PRICE:", '${:,.2f}'.format(avg_price))
 
 
 
